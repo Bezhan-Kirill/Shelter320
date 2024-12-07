6faabc1f0c83883b55e86c476167109030580ed5
Bezhan-Kirill <bezhan_kiril@mail.ru>
2024-12-07 21:29:03 +0300

    Расширенна модель пользователя

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 from users.models import User
 
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone',)
 
 class UserRegisterForm(forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
@@ -16,3 +20,8 @@ class UserRegisterForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Пароли не совпадают!!!')
         return cd['password2']
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
