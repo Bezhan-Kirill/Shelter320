dfd2dc381ba5a5c97c54cd2768cc01c34831699e
Bezhan-Kirill <bezhan_kiril@mail.ru>
2024-12-07 21:28:47 +0300

    Расширенна модель пользователя

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 from django.shortcuts import render, reverse
-
+from django.contrib.auth import authenticate, login, logout
 from users.models import User
-from users.forms import UserRegisterForm
-
+from users.forms import UserRegisterForm, UserLoginForm, UserForm
+from django.contrib.auth.decorators import login_required
 
 def user_register_view(request):
     if request.method == 'POST':
@@ -13,5 +13,31 @@ def user_register_view(request):
             new_user = form.save()
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
-            return HttpResponseRedirect(reverse('dogs:index'))
+            return HttpResponseRedirect(reverse('users:login_user'))
     return render(request, 'users/register_user.html', {'form': UserRegisterForm}, )
+
+def user_login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(email=cd['email'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('dogs:index'))
+            else:
+                return HttpResponse('Аккаунт неактивен!')
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login_user.html', {'form': form})
+
+
+@login_required
+def user_profile_view(request):
+    user_object = request.user
+    context = {
+        # 'user_object': user_object,
+        'title': f'Ваш профиль {user_object.first_name}',
+        # 'form': UserForm(instance=user_object),
+    }
+    return render(request, 'users/user_profile.html', context)
