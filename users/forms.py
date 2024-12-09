d70cea668d77415080952d86bc7ad46386fb7978
Bezhan-Kirill <bezhan_kiril@mail.ru>
2024-12-09 23:13:01 +0300

    Добавлены методы проверки пароля

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 from users.models import User
+from users.validators import validate_password
+
 
 class StyleFormMixin:
 
@@ -9,10 +11,12 @@ class StyleFormMixin:
         for field_name, field in self.fields.item():
             field.widget.attrs['class'] = 'form-control'
 
+
 class UserForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone',)
+
 
 class UserRegisterForm(StyleFormMixin, forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
@@ -23,10 +27,13 @@ class UserRegisterForm(StyleFormMixin, forms.ModelForm):
         fields = ('email',)
 
     def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
+        temp_data = self.cleaned_data
+        print(temp_data)
+        validate_password(temp_data['password'])
+        if temp_data['password'] != temp_data['password2']:
+            print('Пароли не совпадают!!!')
             raise forms.ValidationError('Пароли не совпадают!!!')
-        return cd['password2']
+        return temp_data['password2']
 
 
 class UserLoginForm(forms.Form):
