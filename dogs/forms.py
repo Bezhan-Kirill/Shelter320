0d5ddd4fb2622c25d305c39fe3df5e1b7a6c42c7
Bezhan-Kirill <bezhan_kiril@mail.ru>
2024-12-18 11:44:37 +0300

    обновлена форма

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+import datetime
 from dogs.models import Dog
 from users.forms import StyleFormMixin
 
@@ -8,3 +8,18 @@ class DogForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dog
         fields = '__all__'
+
+class DogForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Dog
+        exclude = ('owner',)
+
+    def clean_birth_date(self):
+        cleaned_data = self.cleaned_data['birth_date']
+        now_year =datetime.datetime.now().year
+        if now_year - cleaned_data.year > 100:
+            raise forms.ValidationError('Собака должна быть моложе 100 лет')
+# class DogUpdateForm(StyleFormMixin, forms.ModelForm):
+#     class Meta:
+#         model = Dog
+#         exclude = ('owner',)
