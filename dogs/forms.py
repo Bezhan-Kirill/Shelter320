58749aff032c2c379a767594d5f79bbc6d43034a
Bezhan-Kirill <bezhan_kiril@mail.ru>
2024-12-20 20:57:48 +0300

    .

--- dogs/forms.py
+++ dogs/forms.py
@@ -16,7 +16,7 @@ class DogForm(StyleFormMixin, forms.ModelForm):
 
     def clean_birth_date(self):
         cleaned_data = self.cleaned_data['birth_date']
-        now_year =datetime.datetime.now().year
+        now_year = datetime.datetime.now().year
         # if now_year - cleaned_data.year > 100:
         #     raise forms.ValidationError('Собака должна быть моложе 100 лет')
 # class DogUpdateForm(StyleFormMixin, forms.ModelForm):
